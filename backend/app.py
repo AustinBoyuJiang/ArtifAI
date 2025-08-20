d64cae029f4b3d54c8feafb9b1025ad13bd832e5
Game Development Club WVSS <austinjiangboyu@gmail.com>
2025-08-19 17:40:10 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from keras.models import load_model
 import numpy as np
 import cv2
